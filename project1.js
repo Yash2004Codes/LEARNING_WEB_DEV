75ffcf0d5e8959b7714d5482562c8ecb1fd86f03
yash sharma <yash.sharma23@spit.ac.ine.com>
2024-10-13 15:58:24 +0530

    user input page 2

--- project1.js
+++ project1.js
@@ -9,3 +9,27 @@ document.getElementById("btn").onclick
     username =document.getElementById("input1").value;
     document.getElementById("myh1").textContent="welcome "+username;
 }
+
+
+
+let age;
+
+document.getElementById("btn2").onclick
+=function()
+{
+    age = document.getElementById("input2").value;
+    document.getElementById("myh1.2").textContent
+    = username + " is "+ age+" years old";  
+}
+
+let gender;
+
+
+document.getElementById("btn3").onclick
+=function()
+{
+
+    gender =document.getElementById("input3").value;
+    document.getElementById("myh1.3").textContent
+    =username + " is "+ gender;
+}

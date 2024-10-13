a137f699df97936c1bc8f3a07342c64c1f82922b
yash sharma <yash.sharma23@spit.ac.ine.com>
2024-10-13 15:29:41 +0530

    user input web

---
+++ project1.js
@@ -0,0 +1,11 @@
+
+
+
+let username;
+
+document.getElementById("btn").onclick
+= function()
+{
+    username =document.getElementById("input1").value;
+    document.getElementById("myh1").textContent="welcome "+username;
+}
